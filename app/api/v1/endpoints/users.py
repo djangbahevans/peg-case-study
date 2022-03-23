eb5f752e5af77c4b791fea45b81df09893c5be19
Evans Djangbah <djangbahevans@yahoo.com>
2022-03-23 01:59:05 +0000

    Sign up route

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter
+from typing import Any
 
+from app import crud, schemas
+from app.api import deps
+from app.schemas.user import UserCreate
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm.session import Session
 
 router = APIRouter()
 
-@router.post("/")
-def create_user():
-    pass
+
+@router.post("/sign-up", response_model=schemas.UserResponse)
+def create_user(user_req: UserCreate, db: Session = Depends(deps.get_db)) -> Any:
+    """
+    Create new user without the need to be logged in.
+    """
+    user = crud.user.get_by_username(db, username=user_req.username)
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail="The user with this username already exists in the system",
+        )
+    user_in = schemas.UserCreate(**user_req.dict())
+    user = crud.user.create(db, obj_in=user_in)
+    return user
--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.db.base import Base
-from app.db.session import engine
+from app.api.v1.api import api_router
+from app.core.config import settings
 
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +13,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(api_router, prefix=settings.API_V1_STRING)
 
 
 @app.get("/")
